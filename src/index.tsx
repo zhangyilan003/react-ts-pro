98fd0e97ae44efe8da33dd90ed536d2ee6c27cb1
linhaoxin <779893396@qq.com>
2019-05-14 21:01:06 +0800

    测试冲突

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import * as styles from './index.scss'
 import Test from '@components/Test'
-
+// 测试冲突2
 const render = () => {
     ReactDOM.render(
         <Test />,
